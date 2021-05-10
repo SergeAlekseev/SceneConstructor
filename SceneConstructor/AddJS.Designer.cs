dd8b863b8358a790cfa0c6466d43366ff96b5e48
SergeAlekseev <serge.alekseev999@gmail.com>
2021-05-10 22:15:16 +0300

    Add sheme

---
+++ SceneConstructor/AddJS.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace SceneConstructor
+{
+	partial class AddJS
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.tb = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.bSelectModel = new System.Windows.Forms.Button();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.checkBox2 = new System.Windows.Forms.CheckBox();
+			this.checkBox3 = new System.Windows.Forms.CheckBox();
+			this.checkBox4 = new System.Windows.Forms.CheckBox();
+			this.bSelectJS = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(9, 11);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(79, 13);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Имя действия";
+			// 
+			// tb
+			// 
+			this.tb.Location = new System.Drawing.Point(12, 27);
+			this.tb.Name = "tb";
+			this.tb.Size = new System.Drawing.Size(160, 20);
+			this.tb.TabIndex = 3;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(10, 60);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(57, 13);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "JS скрипт";
+			this.label2.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// bSelectModel
+			// 
+			this.bSelectModel.Location = new System.Drawing.Point(13, 76);
+			this.bSelectModel.Name = "bSelectModel";
+			this.bSelectModel.Size = new System.Drawing.Size(75, 23);
+			this.bSelectModel.TabIndex = 5;
+			this.bSelectModel.Text = "Выбор..";
+			this.bSelectModel.UseVisualStyleBackColor = true;
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(13, 105);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(56, 17);
+			this.checkBox1.TabIndex = 8;
+			this.checkBox1.Text = "Текст";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			this.checkBox2.AutoSize = true;
+			this.checkBox2.Location = new System.Drawing.Point(13, 128);
+			this.checkBox2.Name = "checkBox2";
+			this.checkBox2.Size = new System.Drawing.Size(64, 17);
+			this.checkBox2.TabIndex = 9;
+			this.checkBox2.Text = "Файл 1";
+			this.checkBox2.UseVisualStyleBackColor = true;
+			// 
+			// checkBox3
+			// 
+			this.checkBox3.AutoSize = true;
+			this.checkBox3.Location = new System.Drawing.Point(12, 151);
+			this.checkBox3.Name = "checkBox3";
+			this.checkBox3.Size = new System.Drawing.Size(64, 17);
+			this.checkBox3.TabIndex = 10;
+			this.checkBox3.Text = "Файл 2";
+			this.checkBox3.UseVisualStyleBackColor = true;
+			// 
+			// checkBox4
+			// 
+			this.checkBox4.AutoSize = true;
+			this.checkBox4.Location = new System.Drawing.Point(13, 174);
+			this.checkBox4.Name = "checkBox4";
+			this.checkBox4.Size = new System.Drawing.Size(84, 17);
+			this.checkBox4.TabIndex = 11;
+			this.checkBox4.Text = "Положение";
+			this.checkBox4.UseVisualStyleBackColor = true;
+			// 
+			// bSelectJS
+			// 
+			this.bSelectJS.Location = new System.Drawing.Point(121, 218);
+			this.bSelectJS.Name = "bSelectJS";
+			this.bSelectJS.Size = new System.Drawing.Size(75, 23);
+			this.bSelectJS.TabIndex = 12;
+			this.bSelectJS.Text = "Добавить";
+			this.bSelectJS.UseVisualStyleBackColor = true;
+			// 
+			// AddJS
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(208, 253);
+			this.Controls.Add(this.bSelectJS);
+			this.Controls.Add(this.checkBox4);
+			this.Controls.Add(this.checkBox3);
+			this.Controls.Add(this.checkBox2);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.bSelectModel);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.tb);
+			this.Name = "AddJS";
+			this.Text = "AddJS";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox tb;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button bSelectModel;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.CheckBox checkBox2;
+		private System.Windows.Forms.CheckBox checkBox3;
+		private System.Windows.Forms.CheckBox checkBox4;
+		private System.Windows.Forms.Button bSelectJS;
+	}
+}
---
+++ SceneConstructor/EditorScene.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SceneConstructor
+{
+	public partial class EditorScene : Form
+	{
+		public EditorScene()
+		{
+			InitializeComponent();
+		}
+	}
+}
